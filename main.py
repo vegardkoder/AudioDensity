44efeb5a80a43931b620850c020853a65b799c49
Vegard Hansen Stenberg <vegard.koder@gmail.com>
2020-11-24 18:21:17 +0100

    It can now play songs

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import simpleaudio.functionchecks as sa
+import simpleaudio as sa
 
-wave_obj = sa.WaveObject.from_wave_file("")
+song_url = "The Wind - Cat Stevens.wav"
+
+wave_obj = sa.WaveObject.from_wave_file(song_url)
 play_obj = wave_obj.play()
 play_obj.wait_done()
